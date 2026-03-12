490a8075b3b1c56353c4381fe85a296797eb4804
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-12 22:20:27 +0530

    Revesion

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def studentView(request):
+      students={
+            "name":"Ayush",
+            "age":18,
+            "college":"GNIOT",
+            }
+      
+      return JsonResponse(students)
