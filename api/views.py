db87a2e4c0d8e0ebe699d18e7b942dcb6f4d7396
Ayush Gautam <ayushstudy647@gmail.com>
2026-04-01 12:05:45 +0530

    Revesion

--- api/views.py
+++ api/views.py
@@ -1,11 +1,28 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from students.models import Students
+from . serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+
 
 # Create your views here.
+@api_view
 def studentView(request):
-      students={
-            "name":"Ayush",
-            "age":18,
-            "college":"GNIOT",
-            }
-      
-      return JsonResponse(students)
+      if(request.method == "GET"):
+            students=Students.objects.all()
+            serializer=StudentSerializer(students,many=True)
+            return Response(serializer.data, status=HTTP_200_OK)
+      elif(request.method=="POST"):
+                        serializer=StudentSerializer(data=request.data)
+                        if(serializer.is_valid()):
+                                serializer.save()
+                                return Response (serializer.data,status=status.HTTP_201_CREATED)
+                        return Response(serializer.errors,status=status.HTTP_404P_BAD_Request)
+
+
+
+
+
+# 76,209,8014,303,11,
--- students/views.py
+++ students/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework.status import HTTP_200_OK
+from . models import Students
+from api.serializers import StudentSerializer
+from rest_framework.status import HTTP_200_OK
+
+
 
 # Create your views here.
 def students(request):
-      return HttpResponse("Helloo User!")
+      students = Students.objects.all()
+      serializer=StudentSerializer(students,many=True)
+      return JsonResponse(serializer.data, status=HTTP_200_OK)
